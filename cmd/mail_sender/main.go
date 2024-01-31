e0f48b00b32332bafd59195949c3f13a239cc3f6
Kootkh <veprikov.s@gmail.com>
2024-01-31 23:49:19 +0300

    2

--- cmd/mail_sender/main.go
+++ cmd/mail_sender/main.go
@@ -3,6 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
+
+	"bytes"
+	"encoding/base64"
+	"mime/multipart"
+	"net/http"
+	"net/smtp"
+	"os"
+	"path/filepath"
 )
 
 const (
@@ -57,6 +66,16 @@ func init() {
 
 }
 
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
+type Sender struct {
+	auth smtp.Auth
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
 func (to *recipients) String() string {
 	return fmt.Sprintf("%s", *to)
 }
@@ -64,6 +83,9 @@ func (to *recipients) String() string {
 func (attach *attachments) String() string {
 	return fmt.Sprintf("%s", *attach)
 }
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 
 func (to *recipients) Set(value string) error {
 	if len(value) < 3 {
@@ -75,6 +97,72 @@ func (to *recipients) Set(value string) error {
 	return nil
 }
 
+func NewSender() *Sender {
+	auth := smtp.PlainAuth("", username, password, host)
+	return &Sender{auth}
+}
+
+func NewMessage(s, b string) *Message {
+	return &Message{Subject: s, Body: b, Attachments: make(map[string][]byte)}
+}
+
+func (m *Message) AttachFile(src string) error {
+	b, err := os.ReadFile(src)
+	if err != nil {
+		return err
+	}
+
+	_, fileName := filepath.Split(src)
+	m.Attachments[fileName] = b
+	return nil
+}
+
+func (m *Message) ToBytes() []byte {
+	buf := bytes.NewBuffer(nil)
+	withAttachments := len(m.Attachments) > 0
+	buf.WriteString(fmt.Sprintf("Subject: %s\n", m.Subject))
+	buf.WriteString(fmt.Sprintf("To: %s\n", strings.Join(m.To, ",")))
+	if len(m.CC) > 0 {
+		buf.WriteString(fmt.Sprintf("Cc: %s\n", strings.Join(m.CC, ",")))
+	}
+
+	if len(m.BCC) > 0 {
+		buf.WriteString(fmt.Sprintf("Bcc: %s\n", strings.Join(m.BCC, ",")))
+	}
+
+	buf.WriteString("MIME-Version: 1.0\n")
+	writer := multipart.NewWriter(buf)
+	boundary := writer.Boundary()
+	if withAttachments {
+		buf.WriteString(fmt.Sprintf("Content-Type: multipart/mixed; boundary=%s\n", boundary))
+		buf.WriteString(fmt.Sprintf("--%s\n", boundary))
+	} else {
+		buf.WriteString("Content-Type: text/plain; charset=utf-8\n")
+	}
+
+	buf.WriteString(m.Body)
+	if withAttachments {
+		for k, v := range m.Attachments {
+			buf.WriteString(fmt.Sprintf("\n\n--%s\n", boundary))
+			buf.WriteString(fmt.Sprintf("Content-Type: %s\n", http.DetectContentType(v)))
+			buf.WriteString("Content-Transfer-Encoding: base64\n")
+			buf.WriteString(fmt.Sprintf("Content-Disposition: attachment; filename=%s\n", k))
+
+			b := make([]byte, base64.StdEncoding.EncodedLen(len(v)))
+			base64.StdEncoding.Encode(b, v)
+			buf.Write(b)
+			buf.WriteString(fmt.Sprintf("\n--%s", boundary))
+		}
+
+		buf.WriteString("--")
+	}
+
+	return buf.Bytes()
+}
+
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
 func (attach *attachments) Set(value string) error {
 	if len(value) < 3 {
 		return fmt.Errorf("attach is too short")
@@ -85,14 +173,22 @@ func (attach *attachments) Set(value string) error {
 	return nil
 }
 
+// ------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
+
 // main description of the Go function.
 //
 // No parameters.
 // No return type.
 func main() {
 
-	var to recipients
-	var attach attachments
+	sender := NewSender()
+	m := NewMessage("Test", "Body message.")
+	m.To = []string{"to@gmail.com"}
+	m.CC = []string{"copy1@gmail.com", "copy2@gmail.com"}
+	m.BCC = []string{"bc@gmail.com"}
+	m.AttachFile("/path/to/file")
+	fmt.Println(sender.Send(m))
 
 	flag.Parse()
 
